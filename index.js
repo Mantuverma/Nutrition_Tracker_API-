bf08f64179300423795dfb5ad2893437d4ded80b
mantu kumar <mantukumarverma2004@gmail.com>
2024-02-10 10:21:26 +0530

    db connection successfully

---
+++ index.js
@@ -0,0 +1,17 @@
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import connectDB from "./db/dbConnection.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 600, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("mongo db connection faild !!!", err)
+    })
---
+++ utils/constant.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "Nutrition Tracker API";
